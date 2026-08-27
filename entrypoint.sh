1da537025b93781588b784e825cd040713b63da9
Andy Baranov <andrey@hey.com>
2026-08-27 11:07:47 +0200

    Simplify visitor welcome copy

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
-cat << EOF > canihackit.hack
-
-You can try! Fork bombs and excessive memory allocations should not work though. Containers also don't have any network. If you manage to break out of this container into a host system—please be kind and shoot me an email at andrey@lewagon.org to explain how you did it :)
-
-EOF
-
 printf '\033[1;38;5;212m'
 cat << 'EOF'
 ████  ████   ███   ████  ███  ████   ███  █   █ ████   ███         ███  ████   ████
@@ -17,8 +11,7 @@ EOF
 printf '\033[0m\033[1;38;5;81mPORTFOLIO / TUI\033[0m  \033[38;5;245m● ONLINE\033[0m\n'
 echo ""
 printf '\033[38;5;255mWelcome, stranger.\033[0m Your private, network-less container is ready.\n'
-printf '\033[38;5;81mPress Enter\033[0m to launch the pre-filled \033[1;38;5;212m./hello2\033[0m command.\n'
-printf '\033[38;5;245mWant the 2020 original instead? Replace it with ./hello.\033[0m\n'
+printf '\033[38;5;81mPress Enter\033[0m to continue.\n'
 echo ""
 
 exec "$@"
